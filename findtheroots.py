9c6e5f5cb4d3a54b808f9b602b06a5739dd51869
ViniciusLeitedoCarmo <87040226+ViniciusLeitedoCarmo@users.noreply.github.com>
2021-09-28 19:42:39 -0300

    Improving

--- findtheroots.py
+++ findtheroots.py
@@ -2,6 +2,8 @@ print("Finding the roots of a quadratic equation!")
 print("Insert the equation's terms in the order of their degrees and follow the instructions.")
 import math
 a=int(input("Enter the first term's coefficient: "))
+while a==0:
+  a=int(input("If it is equal to zero, it's not a quadratic equation and we can't make the calculation. Insert a value different from 0: "))
 b=int(input("Enter the second term's coefficient: "))
 c=int(input("Enter the third term's coefficient: "))
 delta=b**2-4*a*c
